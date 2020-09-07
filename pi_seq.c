d80db230fca9702a972d089c0a26e08e28f7b1fe
walterss22 <56270150+walterss22@users.noreply.github.com>
2020-09-06 21:02:41 -0400

    edit

--- pi_seq.c
+++ pi_seq.c
@@ -10,24 +10,26 @@ This program sequentially estimates pi.
 #include <stdint.h>
 #include <time.h>
 
-double find_pi(){
+double find_pi(long terms ){
     //C doesn't actually have Pi as a constant, but...
     //Most modern CPUs actually have an instruction to just load Pi into a register!
     //Some inline assembly here. This should work for all versions of GCC...
     //*** Author: Kefu Lu
-    double Pi;
-    asm("fldpi \n\t"
-        "fstpl %0"
-        : "=m"(Pi));
+    double Pi = 4;
+    for (int i = 0; i < terms)
+
     return Pi; 
 }
 
 int main(int argc, char** argv){
     struct timespec start, end; //structs used for timing purposes, it has two memebers, a tv_sec which is the current second, and the tv_nsec which is the current nanosecond.
     double time_diff;
+    if argv.length > 1{
+        long terms = (long) argv[1];
+    }
     
     clock_gettime(CLOCK_MONOTONIC, &start); //Start the clock!
-    double pi = find_pi();
+    double pi = find_pi(argc);
     clock_gettime(CLOCK_MONOTONIC, &end);   //Stops the clock!
 
     time_diff = (end.tv_sec - start.tv_sec); //Difference in seconds
