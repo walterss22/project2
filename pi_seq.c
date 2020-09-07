9453521eaaccf64498d21190daf0086876256dd1
walterss22 <56270150+walterss22@users.noreply.github.com>
2020-09-06 20:46:15 -0400

    Update pi_seq.c

--- pi_seq.c
+++ pi_seq.c
@@ -22,4 +22,19 @@ double find_pi(){
     return Pi; 
 }
 
+int main(int argc, char** argv){
+    struct timespec start, end; //structs used for timing purposes, it has two memebers, a tv_sec which is the current second, and the tv_nsec which is the current nanosecond.
+    double time_diff;
+    
+    clock_gettime(CLOCK_MONOTONIC, &start); //Start the clock!
+    double pi = find_pi();
+    clock_gettime(CLOCK_MONOTONIC, &end);   //Stops the clock!
+
+    time_diff = (end.tv_sec - start.tv_sec); //Difference in seconds
+    time_diff += (end.tv_nsec - start.tv_nsec) / 1e9; //Difference in nanoseconds
+
+    printf("The time taken is %f \n", time_diff);
+    printf("This example doesn't use terms\n");
+    printf("Pi is %.20lf\n", pi);
+}
 
