51d78743a801fa445e0c3cac56508a154df7f57e
walterss22 <56270150+walterss22@users.noreply.github.com>
2020-09-09 08:50:13 -0400

    pi_seq.c now works
    use 1.0 as dividend

--- pi_seq.c
+++ pi_seq.c
@@ -7,20 +7,23 @@ This program sequentially estimates pi.
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdint.h>
-#include <time.h>
+#include <stdint.h> //to use uint_64
+#include <time.h> //to use clock
+#include <math.h> //to use pow function
+
+#define DEFAULT 10
 
 double find_pi(long terms ){
     //C doesn't actually have Pi as a constant, but...
     //Most modern CPUs actually have an instruction to just load Pi into a register!
     //Some inline assembly here. This should work for all versions of GCC...
     //*** Author: Kefu Lu
-    double Pi = 4;
-    double mult = 0;
+    long double Pi = 4;
+    long double mult = 0;
     for (int i = 0; i < terms; i++){
-        mult += pow(-1, i) * (1 / ((2 * i) +1));
+        mult += pow(-1, i) * (1.0 / ((2 * i) + 1));
     }
-
+    printf("multiplier is %Lf\n", mult);
     Pi *= mult;
     return Pi; 
 }
@@ -28,21 +31,23 @@ double find_pi(long terms ){
 int main(int argc, char** argv){
     struct timespec start, end; //structs used for timing purposes, it has two memebers, a tv_sec which is the current second, and the tv_nsec which is the current nanosecond.
     double time_diff;
-    if(argv* >= 1){
-        long terms = (long) argv[1];
+    long terms;
+
+    if(argc >=2){
+        terms = strtoull(argv[1], NULL, 10);
     } else {
-        long terms = 1000000;
+        terms = DEFAULT;
     }
     
     clock_gettime(CLOCK_MONOTONIC, &start); //Start the clock!
-    double pi = find_pi(argc);
+    double pi = find_pi(terms);
     clock_gettime(CLOCK_MONOTONIC, &end);   //Stops the clock!
 
     time_diff = (end.tv_sec - start.tv_sec); //Difference in seconds
     time_diff += (end.tv_nsec - start.tv_nsec) / 1e9; //Difference in nanoseconds
 
     printf("The time taken is %f \n", time_diff);
-    printf("This example doesn't use terms\n");
+    printf("%ld terms used.\n", terms);
     printf("Pi is %.20lf\n", pi);
 }
 
