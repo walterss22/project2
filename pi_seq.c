02f8f9b54ebefd04895042e8b5d8d1993aaeec44
walterss22 <56270150+walterss22@users.noreply.github.com>
2020-09-09 09:24:43 -0400

    add comments

--- pi_seq.c
+++ pi_seq.c
@@ -17,14 +17,14 @@ double find_pi(long terms ){
     //C doesn't actually have Pi as a constant, but...
     //Most modern CPUs actually have an instruction to just load Pi into a register!
     //Some inline assembly here. This should work for all versions of GCC...
-    long double Pi = 4;
-    long double mult = 0;
+    long double Pi = 4; //initialize Pi
+    long double mult = 0; //initialize multiplier
     for (int i = 0; i < terms; i++){
-        mult += pow(-1, i) * (1.0 / ((2 * i) + 1));
+        mult += pow(-1, i) * (1.0 / ((2 * i) + 1)); //use estimation algo
     }
-    printf("multiplier is %Lf\n", mult);
-    Pi *= mult;
-    return Pi; 
+    // printf("multiplier is %Lf\n", mult); //used for checking multiplier
+    Pi *= mult; //finish estimation
+    return Pi;  //return Pi
 }
 
 int main(int argc, char** argv){
@@ -32,12 +32,14 @@ int main(int argc, char** argv){
     double time_diff;
     long terms;
 
+    //gather command line arg
     if(argc >=2){
         terms = strtoull(argv[1], NULL, 10);
     } else {
         terms = DEFAULT;
     }
     
+    //get time taken
     clock_gettime(CLOCK_MONOTONIC, &start); //Start the clock!
     double pi = find_pi(terms);
     clock_gettime(CLOCK_MONOTONIC, &end);   //Stops the clock!
@@ -45,6 +47,7 @@ int main(int argc, char** argv){
     time_diff = (end.tv_sec - start.tv_sec); //Difference in seconds
     time_diff += (end.tv_nsec - start.tv_nsec) / 1e9; //Difference in nanoseconds
 
+    //print results
     printf("The time taken is %f \n", time_diff);
     printf("%ld terms used.\n", terms);
     printf("Pi is %.20lf\n", pi);
